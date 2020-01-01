9a3e4d463c02663522eccc06a6a3df7180a72254
Akash Patel <43804381+akkipatel21@users.noreply.github.com>
2020-01-01 20:48:30 +0530

    Add files via upload

---
+++ AdminController.java
@@ -0,0 +1,264 @@
+package com.controller.adminPage;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.Session;
+import org.springframework.expression.ParseException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dao.AdminRpt_DAO;
+import com.dao.AdminRpt_DAOImpl;
+import com.dao.CreateUserDAO;
+import com.dao.CreateUserDAOImpl;
+import com.dao.DeskVeri_DAO;
+import com.dao.DeskVeri_DAOImpl;
+import com.dao.UserLoginDAO;
+import com.dao.UserLoginDAOImpl;
+import com.models.AdminLogin;
+import com.models.ApplicationFormDDANew;
+import com.models.AssignAssembly;
+import com.models.Colony_Master_Model;
+import com.models.UserLogin;
+import com.models.desk_deficiency;
+import com.persistance.util.HibernateUtil;
+import com.persistance.util.HibernateUtilNA;
+
+@Controller
+@RequestMapping(value = {"admin","/" ,"user"}) 
+public class AdminController {	
+	 public CreateUserDAO createUserDAO = new CreateUserDAOImpl();
+	 public UserLoginDAO userLoginDAO = new UserLoginDAOImpl();
+	 public AdminRpt_DAO adminDao = new AdminRpt_DAOImpl();
+	 
+	@RequestMapping(value="/adminPage")
+	public ModelAndView adminPage(ModelMap model,@RequestParam(value = "msg", required = false) String msg,HttpSession session){
+		System.out.println("Admin Page MODULE");
+		int role = Integer.parseInt(session.getAttribute("role").toString());
+		if(role != 1){
+			model.put("msg","You are not Authorise User for this Module!");
+			return new ModelAndView("redirect:login");
+		}
+		model.put("role",role);
+		model.put("msg",msg);
+		return new ModelAndView("adminPageMiduleTile");
+	}
+	
+	@RequestMapping(value="/createUserForm")
+	public ModelAndView createUserForm(ModelMap model,@RequestParam(value = "msg", required = false) String msg,HttpSession session){
+		System.out.println("Create UserForm MODULE");
+		int role = Integer.parseInt(session.getAttribute("role").toString());
+		if(role != 1){
+			model.put("msg","You are not Authorise User for this Module!");
+			return new ModelAndView("redirect:login");
+		}
+		model.put("msg",msg);
+		return new ModelAndView("createUserFormTile","userLogin",new UserLogin());
+	}
+	
+	
+	
+	@RequestMapping(value = "/admin/locationGisModule", method = RequestMethod.GET)
+	public ModelAndView locationGisModulePage() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("locationGisModulePage");
+		return model;
+	}
+	
+	@RequestMapping(value = "/Vision", method = RequestMethod.GET)
+	public ModelAndView Vision() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("VisionTile");
+		return model;
+	}
+
+	
+	
+	@RequestMapping(value = "/HelpDesk", method = RequestMethod.GET)
+	public ModelAndView Helpdesk() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("HelpDeskTile");
+		return model;
+	}
+	
+	
+	@RequestMapping(value = "/Processing_Center", method = RequestMethod.GET)
+	public ModelAndView Processing_Center() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("ProcessingCenterTile");
+		return model;
+	}
+	@RequestMapping(value = "/Instructions", method = RequestMethod.GET)
+	public ModelAndView Instructions() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("instructionsTile");
+		return model;
+	}
+	@RequestMapping(value = "/createUser", method = RequestMethod.GET)
+	public ModelAndView createUser(@RequestParam(value="msg",required =false)String msg,ModelMap modelMap) {
+		ModelAndView model = new ModelAndView();
+		modelMap.put("msg",msg);
+//		model.setViewName("createUserTile");
+		return new ModelAndView("createUserTile");
+	}
+	@RequestMapping(value = "/colonyAssignment", method = RequestMethod.GET)
+	public ModelAndView colonyAssginment() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("colonyAssignmentTile");
+		return model;
+	}
+
+	
+	@RequestMapping(value = "/saveCreateUser", method = RequestMethod.POST)
+	public ModelAndView saveCreateUser (@ModelAttribute("createUser")AdminLogin adminLogin,ModelMap model) {
+		Session session11 = HibernateUtil.getSessionFactory().openSession();
+		session11.beginTransaction();
+		session11.save(adminLogin);
+		session11.getTransaction().commit();
+		session11.close();
+		String msg = ("New "+adminLogin.getType()+" created");
+		model.put("msg", msg);
+		return new ModelAndView("redirect:createUser");
+	}
+	
+	@RequestMapping(value = "/getUserNames")
+	public @ResponseBody List<AdminLogin> getUserNames (String userType) {
+		return adminDao.getUserNamesByUserType(userType);
+	}
+	@RequestMapping(value = "/getAssemblyList")
+	public @ResponseBody List<Colony_Master_Model> getAssemblyList (String userType) {
+		return adminDao.getAssemblyList(userType);
+	}
+	
+	@RequestMapping(value = "/assignAssembly",method = RequestMethod.POST)
+	@ResponseBody
+	public ModelAndView assignAssembly(HttpServletRequest request,
+	@ModelAttribute("colonyAssignment") AssignAssembly assignAssembly, ModelMap model,@RequestParam(value = "msg", required = false) String msg, HttpSession session) throws ParseException, IOException  {
+		
+		int userId = adminDao.getUserIdByUserName(assignAssembly.getUsername());
+		System.out.println(assignAssembly.getUsername());
+		System.out.println(assignAssembly.getType());
+		String[] defcount=request.getParameterValues("fields_count");
+		int v=Integer.parseInt(defcount[0]);
+		for (int i=1;i<=v;i++)
+		{
+			String[] assembly = request.getParameterValues("assembly"+i);
+			String abc = assembly[0];
+			List<Colony_Master_Model> list = adminDao.getColoniesByAssemblyName(abc);
+			for (Colony_Master_Model colony_Master_Model : list) {
+			assignAssembly.setAssembly(abc);
+			assignAssembly.setType(assignAssembly.getType());
+			assignAssembly.setUser_id(userId);
+			assignAssembly.setColony_reg_no(colony_Master_Model.getReg_no());
+			assignAssembly.setAssembly_no(colony_Master_Model.getAssembly_no());
+			assignAssembly.setDistrict(colony_Master_Model.getDistrict());
+			Session session1 = HibernateUtilNA.getSessionFactory().openSession(); 
+			session1.beginTransaction(); 
+			session1.save(assignAssembly);
+			session1.getTransaction().commit();
+			session1.close();
+			}
+		}
+		
+		return new ModelAndView("redirect:colonyAssignment");
+	}
+	
+	
+
+
+	//server path
+	
+//  private String server = "10.247.102.61";
+	private String server = "192.168.1.196"; 
+	private int port = 8080;
+		
+				@RequestMapping("/wms")
+				@ResponseBody
+				public String mirrorRest(@RequestBody String body, HttpMethod method, HttpServletRequest request,
+				    HttpServletResponse response) throws URISyntaxException
+				{
+					//System.out.println("sadasdsadhsdsd"+request.getQueryString());
+				//	String temp = new String(request.getQueryString());
+				String req =	request.getQueryString().replace("%2C", ",").replace("%3A", ":").replace("%2F", "/").replace("%3D", "=").replace("%5B", "[").replace("%5D","]").replace("%27", "'").replace("%20", " ").replace("%3C", "<").replace("%3E", ">");
+					
+
+					
+				    URI uri = new URI("http", null, server, port,"/geoserver/cite/wms",req , null);
+				
+				  //  System.out.println(uri);
+				    RestTemplate restTemplate = new RestTemplate();
+				    ResponseEntity<String> responseEntity =
+				        restTemplate.exchange(uri, method, new HttpEntity<String>(body), String.class);
+
+				    return responseEntity.getBody();
+				}
+				
+				
+				@RequestMapping("/ows")
+				@ResponseBody
+				public String mirrorRestOWS(@RequestBody String body, HttpMethod method, HttpServletRequest request,
+				    HttpServletResponse response) throws URISyntaxException
+				{
+					//System.out.println("sadasdsadhsdsd"+request.getQueryString());
+				//	String temp = new String(request.getQueryString());
+				String req =	request.getQueryString().replace("%2C", ",").replace("%3A", ":").replace("%2F", "/").replace("%27", "'").replace("%20", " ");
+					
+
+					
+				    URI uri = new URI("http", null, server, port,"/geoserver/cite/ows",req , null);
+				
+				  //  System.out.println(uri);
+				    RestTemplate restTemplate = new RestTemplate();
+				    ResponseEntity<String> responseEntity =
+				        restTemplate.exchange(uri, method, new HttpEntity<String>(body), String.class);
+
+				    return responseEntity.getBody();
+				}
+				
+				
+				@RequestMapping(value="/createDistrictUserForm")
+				public ModelAndView createDistrictUserForm(ModelMap model,@RequestParam(value = "msg", required = false) String msg,HttpSession session){
+					System.out.println("Create District UserForm MODULE");
+					int role = Integer.parseInt(session.getAttribute("role").toString());
+					String state = session.getAttribute("state").toString();
+					if(role != 3){
+						model.put("msg","You are not Authorise User for this Module!");
+						return new ModelAndView("redirect:login");
+					}
+					System.out.println("state==" + state);
+					model.put("state",state);
+					model.put("msg",msg);
+					return new ModelAndView("createDistrictUserFormTile","userLogin",new UserLogin());
+				}
+				
+				
+				@RequestMapping(value = "/adminModule3", method = RequestMethod.GET)
+				public ModelAndView adminModule3() {
+					ModelAndView model = new ModelAndView();
+					model.setViewName("adminModule3Tile");
+					return model;
+				}
+
+}
---
+++ colonyAssignment.jsp
@@ -0,0 +1,203 @@
+
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<script src="js/Deskadmin/deskadmin.js"></script>
+
+
+<style>
+
+/* The Modal (background) */
+.modal {
+	display: none; /* Hidden by default */
+	position: fixed; /* Stay in place */
+	z-index: 1; /* Sit on top */
+	padding-top: 100px; /* Location of the box */
+	left: 0;
+	top: 0;
+	width: 100%; /* Full width */
+	height: 100%; /* Full height */
+	overflow: auto; /* Enable scroll if needed */
+	background-color: rgb(0, 0, 0); /* Fallback color */
+	background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
+}
+
+.mar {
+	margin: 18px;
+}
+
+.len {
+	right: 1232px;
+}
+
+.rej {
+	background-color: #f24636;
+	color: white;
+}
+
+.rej:hover {
+	color: maroon;
+	text-decoration: none;
+	cursor: pointer;
+}
+
+/* Modal Content */
+.modal-content {
+	background-color: #fefefe;
+	margin: auto;
+	padding: 20px;
+	border: 1px solid #888;
+	width: 80%;
+}
+
+/* The Close Button */
+.close {
+	color: #aaaaaa;
+	float: right;
+	font-size: 28px;
+	font-weight: bold;
+}
+
+.close:hover, .close:focus {
+	color: #000;
+	text-decoration: none;
+	cursor: pointer;
+}
+
+.panel-head {
+	text-align: center;
+}
+</style>
+<script src="js/Responsive/bootstrap.js"></script>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+	getAssemblyList()
+	var msg="${msg}";
+	if(msg){
+		alert(msg);
+	}
+	
+	});
+	
+	
+	function getUserNames(){
+		
+		var userType=document.getElementById("type").value;
+		var options="";
+		$.getJSON("getUserNames",{userType:userType},function(j) {
+			
+			if(j.length < 0){
+				alert("No UserNames Allocated to "+userType+" Type");
+				return false;
+			}
+			options = '<option value="' + "--Select--" + '">'+ "--Select--" + '</option>';
+			for ( var i = 0; i < j.length; i++) {
+					options += '<option value="' + j[i]+ '" name="' + j[i]+ '">'+ j[i] + '</option>';
+			}				
+			$("select#userNames").html(options);	
+			
+		});
+	}
+	
+	function getAssemblyList(){
+		var options="";
+		$.getJSON("getAssemblyList",function(j) {
+			
+			options = '<option value="' + "--Select--" + '">'+ "--Select--" + '</option>';
+			for ( var i = 0; i < j.length; i++) {
+					options += '<option value="' + j[i]+ '" name="' + j[i]+ '">'+ j[i] + '</option>';
+			}				
+			$("select#assembly").html(options);	
+			
+		});
+	}
+	 var fields=0;
+	  function addColony()
+	  {	
+		 var a="";
+		fields++;
+		var userType=document.getElementById("assembly").value;
+	  	 document.getElementById("fields").value=fields;
+	  	$("table#colonyDetails").append(
+	      		'<tr id="'+fields+'"><td>Assembly-'+fields+'</td>'	
+	      		+'<td><input class="form-control"  id="deficiency_detail'+fields+'" name="assembly'+fields+'" value="'+userType+'" /></td>' 
+	      		+'</tr>');
+
+	  	///////+'<td><input type="text" name="def'+fields+'" id="def'+fields+'" style="width:60%" /></td>' 
+	  }
+	
+</script>
+
+</head>
+
+<body id="page-top" class="index">
+	<form:form modelAttribute="colonyAssignment"
+		name="createUser" id="createUser" action="assignAssembly"
+		method='POST'>
+		 <input type="hidden" id="fields"  name="fields_count" value="1">
+		<div id="print_window" class="mar">
+			<div class="form-head mar">
+				<h5>colony assignment</h5>
+			</div>
+			<hr>
+			<div class="row form-group mar">
+				<div class="col col-md-6">
+				<div class="col col-md-4">
+                        <label for="text-input" class=" form-control-label">User Type :<span style="color: red;"> *</span></label>
+                      </div>
+                    <div class="col-8 col-md-6">
+						<select id="type" name="type" class="form-control" onchange="getUserNames();">
+							<option value="--Select--">--Select--</option>
+							<option value="ASO">ASO</option>
+							<option value="SURVEY">SURVEY</option>
+							<option value="AD">AD</option>
+							<option value="DD">DD</option>
+							<option value="DIRECTOR">DIRECTOR</option>
+						</select>
+					</div>	
+					</div>
+				<div class="col col-md-2">
+                        <label for="text-input" class=" form-control-label">UserName :<span style="color: red;"> *</span></label>
+                      </div>
+                    <div class="col-12 col-md-3">
+						 <select   class="form-control" id="userNames" name="username">
+                        </select>
+					</div>
+               </div>
+			<div class="row form-group mar">
+				<div class="col col-md-11">
+				<div class="col col-md-2">
+                        <label for="text-input" class=" form-control-label">ASSEMBLY :<span style="color: red;"> *</span></label>
+                      </div>
+                    <div class="col-8 col-md-8">
+						<select id="assembly"  class="form-control">
+							
+						</select>
+					</div>	
+					<a href="#" class="btn btn-success" onclick="addColony()">Add Assembly</a>
+  			</div>
+               </div>
+                      
+                        <table border="1" id="colonyDetails" class="table table-striped">
+ 						<tr>
+						<!--  <td>Deficiency-1</td> -->
+						<!-- <td>Deficiency-1</td> -->
+						<!--  <td><input class="form-control"  id="deficiency_detail1" name="deficiency_detail1" /></td> -->
+						<!--   <td><input name="submit" type="submit" value="Submit"  class="btn btn-info" style="font-size: 15px;width: 150px;"/>&nbsp;</td> -->
+						</tr>
+ 						</table> 
+				<div class="col-md-12">
+					<div class="well well-sm well-primary" align="center">
+						<div class="actions-box">
+
+							<input id="saveData" class="btn btn-primary btn-sm"
+								value="Create User" type="submit"
+								onclick="return validateForm();">
+						</div>
+					</div>
+				</div>
+			</div>
+
+	</form:form>
+</body>
+</html>
